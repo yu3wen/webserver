a6f43037672ec26af39721c722d5a9d8839db956
EavanZhao <729390400@qq.com>
2021-04-15 13:34:12 +0800

    guessnumber

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const Koa = require('koa')
 const Router = require('koa-router')
-var R
-let app = new Koa()
+
+let R
+const app = new Koa()
 app.listen(3000)
-let router = new Router()
-router.get('/start', (ctx, next) => {
+const router = new Router()
+router.get('/start', (ctx) => {
 	ctx.body = 'OK'
-	R = parseInt(100 * Math.random())
-
-
+	R = Math.floor(100 * Math.random())
 })
 
-router.get('/:number', (ctx, next) => {
-	var number = parseInt(ctx.params['number'])
+router.get('/:number', (ctx) => {
+	const number = Math.floor(ctx.params.number)
 	if (number > R) {
 		ctx.body = 'smaller'
-	}
-	else if (number == R) {
+	} else if (number === R) {
 		ctx.body = 'euqal'
-	}
-	else if (number < R) {
+		R = Math.floor(100 * Math.random())
+	} else if (number < R) {
 		ctx.body = 'bigger'
 	}
-	R = parseInt(100 * Math.random())
-
 })
 
 app.use(router.routes())
