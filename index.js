a2b4c2d1e2c8a6531e226ebcc6a821594cf834e2
EavanZhao <729390400@qq.com>
2021-04-16 15:02:18 +0800

    redisnumber

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const client = Redis.createClient(6379, 'localhost')
 let R
 const app = new Koa()
 app.listen(3000)
-app.listen(3001)
-app.listen(3002)
-app.listen(3003)
-app.listen(3004)
+
 const router = new Router()
 
 function getredis() {
@@ -21,9 +18,11 @@ function getredis() {
 }
 
 router.get('/start', async (ctx) => {
-	ctx.body = 'OK'
 	R = Math.floor(100 * Math.random())
-	client.set('Rnum', R)
+	client.set('Rnum', R, (...args) => {
+		console.log(args)
+	})
+	ctx.body = 'OK'
 })
 
 router.get('/:number', async (ctx) => {
@@ -34,8 +33,9 @@ router.get('/:number', async (ctx) => {
 	} else if (number === R) {
 		ctx.body = 'euqal'
 		R = Math.floor(100 * Math.random())
-		client.set('Rnum', R)
-		// await getredis()
+		client.set('Rnum', R, (...args) => {
+			console.log(args)
+		})
 	} else if (number < R) {
 		ctx.body = 'bigger'
 	}
