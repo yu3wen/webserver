aa52e7a44b200ea7fbedab3a8cf4f92b97186f51
EavanZhao <729390400@qq.com>
2021-04-19 15:03:25 +0800

    ejsguess

--- index.js
+++ index.js
@@ -1,45 +1,80 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const Redis = require('redis')
+const views = require('koa-views')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
 
 const client = Redis.createClient(6379, 'localhost')
 let R
+let message
 const app = new Koa()
 app.listen(3000)
-app.listen(3001)
-app.listen(3002)
-app.listen(3003)
-app.listen(3004)
 const router = new Router()
 
+app.use(views(path.join(__dirname, './static'), {
+	extension: 'ejs',
+}))
+
+app.use(bodyParser())
+
 function getredis() {
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		client.get('Rnum', (err, res) => {
-			resolve(Number(res))
+			if (res) resolve(Number(res))
+			else if (err) reject(err)
 		})
 	})
 }
 
-router.get('/start', async (ctx) => {
-	ctx.body = 'OK'
-	R = Math.floor(100 * Math.random())
-	client.set('Rnum', R)
+function setredis(num) {
+	return new Promise((resolve, reject) => {
+		client.set('Rnum', num, (err, res) => {
+			if (res) resolve(res)
+			else if (err) reject(err)
+		})
+	})
+}
+
+router.get('/', async (ctx) => {
+	message = ''
+	await ctx.render('index', {
+		message,
+	})
 })
 
-router.get('/:number', async (ctx) => {
-	const number = Math.floor(ctx.params.number)
-	R = await getredis()
-	if (number > R) {
-		ctx.body = 'smaller'
-	} else if (number === R) {
-		ctx.body = 'euqal'
-		R = Math.floor(100 * Math.random())
-		client.set('Rnum', R)
-		// await getredis()
-	} else if (number < R) {
-		ctx.body = 'bigger'
+router.post('/start', async (ctx) => {
+	message = 'OK'
+	await ctx.render('index', {
+		message,
+	})
+})
+
+router.post('/number', async (ctx) => {
+	const value = ctx.request.body.num || ''
+	try {
+		R = await getredis()
+		if (Number(value) > R) {
+			message = 'small'
+			await ctx.render('index', {
+				message,
+			})
+		} else if (Number(value) === R) {
+			message = 'equal'
+			await ctx.render('index', {
+				message,
+			})
+			R = Math.floor(100 * Math.random())
+			await setredis(R)
+		} else if (Number(value) < R) {
+			message = 'big'
+			await ctx.render('index', {
+				message,
+			})
+		}
+	} catch (err) {
+		console.log(err)
 	}
-	console.log('R', R)
 })
 
 app.use(router.routes())
