67f4d875a6d54cbf8c20db3ade210d792ac7f7fd
EavanZhao <729390400@qq.com>
2021-04-23 09:34:00 +0800

    promise

--- promise.js
+++ promise.js
@@ -1,38 +1,30 @@
-const re = require('request')
+const rp = require('request-promise')
 
-function conn(first, second, cb) {
-	const number = Math.floor((first + second) / 2)
+function conn(parma) {
 	const options = {
 		url: 'http://localhost:3000/number',
 		method: 'post',
 		form: {
-			num: number,
+			num: parma,
 		},
 	}
-	re(options, (err, res, body) => {
-		if (err) cb(err)
-		if (body === 'big') {
-			conn(first, Math.floor((first + second) / 2) - 1, cb)
-		} else if (body === 'small') {
-			conn(Math.floor((first + second) / 2) + 1, second, cb)
-		} else if (body === 'equal') {
-			cb(number)
-		}
-	})
-}
-
-function pro() {
-	return new Promise((resolve, reject) => {
-		conn(0, 1000000, (err, res) => {
-			if (err) reject(err)
-			resolve(res)
+	return rp(options).then((res) => new Promise((resolve) => {
+		resolve(res)
+	}))
+		.catch((err) => {
+			console.log(err)
 		})
-	})
 }
 
-pro().then((res) => {
-	console('num:', res)
-})
-	.catch((err) => {
-		console.log('err:', err)
-	})
+function pro(first, second) {
+	const parma = Math.floor((first + second) / 2)
+	return new Promise((resolve) => {
+		conn(parma).then((res) => {
+			if (res === 'big') pro(first, Math.floor((first + second) / 2) - 1)
+			else if (res === 'small') pro(Math.floor((first + second) / 2) + 1, second)
+			else if (res === 'equal') resolve(parma)
+		})
+	}).then((result) => { console.log('number:', result) })
+}
+
+pro(0, 1000000)
