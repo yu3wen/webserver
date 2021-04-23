554acc038542320a7a499e113e4e820ca87df9ed
EavanZhao <729390400@qq.com>
2021-04-23 11:05:23 +0800

    promise

--- promise.js
+++ promise.js
@@ -8,23 +8,25 @@ function conn(parma) {
 			num: parma,
 		},
 	}
-	return rp(options).then((res) => new Promise((resolve) => {
-		resolve(res)
-	}))
-		.catch((err) => {
-			console.log(err)
-		})
+	return rp(options)
 }
 
 function pro(first, second) {
 	const parma = Math.floor((first + second) / 2)
-	return new Promise((resolve) => {
-		conn(parma).then((res) => {
-			if (res === 'big') pro(first, Math.floor((first + second) / 2) - 1)
-			else if (res === 'small') pro(Math.floor((first + second) / 2) + 1, second)
-			else if (res === 'equal') resolve(parma)
+	conn(parma).then((res) => {
+		if (res === 'big') {
+			pro(first, Math.floor((first + second) / 2) - 1)
+			return
+		} if (res === 'small') {
+			pro(Math.floor((first + second) / 2) + 1, second)
+			return
+		} if (res === 'equal') {
+			console.log('number:', parma)
+		}
+	})
+		.catch((err) => {
+			console.log('err', err)
 		})
-	}).then((result) => { console.log('number:', result) })
 }
 
 pro(0, 1000000)
